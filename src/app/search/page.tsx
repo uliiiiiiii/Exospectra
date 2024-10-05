46174342a60d4fa8fc42bbf5058433c8ef8a67ba
uliiiiiiii <56516009+uliiiiiiii@users.noreply.github.com>
2024-10-05 23:08:41 +0300

    'search for an object' functionality

---
+++ src/app/search/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Suspense } from 'react';
+import searchObject from '../utils/searchObject';
+
+function Search() {
+    const searchParams = useSearchParams();
+    const planetName = searchParams.get('object');
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter()
+
+    useEffect(() => {
+        if (planetName) {
+            setLoading(true);
+            searchObject(planetName).then((data) => {
+                setLoading(false);
+                if (data) {
+                    if (data.type === "exoplanet") {
+                        alert('found an exoplanet')
+                        router.push(`/exoplanet?name=${encodeURIComponent(planetName)}`);
+                    } else if (data.type === "star") {
+                        alert('found system')
+                        router.push(`/system?name=${encodeURIComponent(planetName)}`);
+                    }
+                }
+                else {
+                    alert("Couldn't find this object. Try searching for something other");
+                    router.back()
+                }
+            })
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+    return (
+        <div>
+            {!planetName ? (
+                <h1>No object name provided.</h1>
+            ) : (<>
+                {loading ? <p>Loading...</p>
+                    : <p>Loaded (this page will be used for preloading and calculation of all info which will be used in the ExoplanetSystem or Exoplanet model)</p>}
+            </>
+            )}
+        </div>
+    );
+}
+
+export default function SearchResults() {
+    return (
+        <Suspense fallback={<p>Loading search params...</p>}>
+            <Search />
+        </Suspense>
+    )
+}
