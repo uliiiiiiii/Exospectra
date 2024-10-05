0889a7f0b7aa9a5c4dcd95bf7547068cf1453ae5
uliiiiiiii <56516009+uliiiiiiii@users.noreply.github.com>
2024-10-06 02:15:33 +0300

    desgin for the page of the exoplanet

--- src/app/exoplanet/page.tsx
+++ src/app/exoplanet/page.tsx
@@ -7,6 +7,9 @@ import css from './page.module.css';
 import StarsPlaceholder from '../components/starsPlaceholder';
 import ExoplanetModel from './components/exoplanet';
 import { PlanetProps } from '@/types/planet';
+import { Html } from "@react-three/drei";
+import ExospectraLabel from '../components/ExospectraLabel';
+import Image from 'next/image';
 
 function ExoplanetSearchResult() {
     const searchParams = useSearchParams();
@@ -55,6 +58,26 @@ function ExoplanetSearchResult() {
                                 position: [0, 0, 5],
                             }}
                         >
+                            <Html fullscreen>
+                                <div className={css.htmlContent}>
+                                    <div className={css.infoBlock}>
+                                        <p>Search Results for: {planetName}</p>
+                                        <ul className={css.dataBlock}>
+                                            <li>Mass (Earth Masses): {planetData.mass}</li>
+                                            <li>Radius (kilometers): {planetData.radius}</li>
+                                            <li>Type: {planetData.type}</li>
+                                            <li>Orbital period (days): {planetData.orbitalPeriod}</li>
+                                        </ul>
+                                    </div>
+                                    <div className={css.other}>
+                                        <ExospectraLabel />
+                                        <div className={css.dragTool}>
+                                            <Image src="/rocket.png" width={90} height={90} alt="rocket" />
+                                            <p>Drag the spaceship to the location where you want to see the sky</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Html>
                             <ambientLight intensity={0.5} />
                             <directionalLight position={[10, 10, 10]} intensity={1} castShadow />
 
