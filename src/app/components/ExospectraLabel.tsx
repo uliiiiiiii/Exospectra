9f198da7996f087eef9243f2a4f2afd18bce9130
uliiiiiiii <56516009+uliiiiiiii@users.noreply.github.com>
2024-10-06 02:18:43 +0300

    disable selection for ExospectraLabel

--- src/app/components/ExospectraLabel.tsx
+++ src/app/components/ExospectraLabel.tsx
@@ -3,9 +3,8 @@ import styles from '../page.module.css';
 import { useRouter } from 'next/navigation';
 
 export default function ExospectraLabel() {
-    const router = useRouter()
     return (
-        <div className={styles.labelContainer} onClick={() => { router.push('/') }}>
+        <div className={styles.labelContainer}>
             <span className={styles.labelText}>EXOSPECTRA</span>
         </div>
     );
