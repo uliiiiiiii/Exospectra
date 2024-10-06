eb6f357075bdaab11f86023cb539e35989f8d406
uliiiiiiii <56516009+uliiiiiiii@users.noreply.github.com>
2024-10-06 05:08:08 +0300

    added Earth icon to our logo

--- src/app/components/ExospectraLabel.tsx
+++ src/app/components/ExospectraLabel.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from '../page.module.css';
-import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function ExospectraLabel() {
     return (
         <div className={styles.labelContainer}>
-            <span className={styles.labelText}>EXOSPECTRA</span>
+            <span className={styles.labelText}>EX<Image src='/earthIcon.png' width={28} height={28} alt='.' />SPECTRA</span>
         </div>
     );
 }
